b30ca8b6c0d36ba6ca580087f5e4e798809b86ba
jam993 <jam993@drexel.edu>
2024-02-19 11:46:51 -0500

    made changes to random.html, the js, and CSS for the randomizer

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function getRandomImage() {  
+ 
+    var randomImage = new Array();  
+      
+    randomImage[0] = "https://i0.wp.com/billypenn.com/wp-content/uploads/2021/11/phillyzoo-luminature2021-lightshow-01crop.jpg?fit=2400%2C1350&ssl=1";  
+    randomImage[1] = "https://www.visitphilly.com/wp-content/uploads/2017/12/First_Friday_Old_City_A.Ricketts1-2200x1237vp.jpg";  
+    randomImage[2] = "https://www.visitphilly.com/wp-content/uploads/2017/12/Chinatown_arch2_G.Widman_2200x1237vp.jpg";  
+    randomImage[3] = "https://www.visitphilly.com/wp-content/uploads/2021/05/blue-cross-riverrink-summerfest-above-new-jfusco-for-vp-2200x1237px-1400x787.jpg";  
+    randomImage[4] = "https://media.cntraveler.com/photos/5a8da73899c77f4533dfbaec/16:9/w_2560,c_limit/Philadelphia-Museum-of-Art__2018_ext-7.jpg";  
+    randomImage[5] = "https://www.visitphilly.com/wp-content/uploads/2017/12/academy-of-natural-sciences-dinosaur-hall-new-crtsy-mike-servedio-ans-2200x1237px.jpg";  
+       
+    var number = Math.floor(Math.random()*randomImage.length);  
+    return document.getElementById("randImg").innerHTML = '<img src="'+randomImage[number]+'" />';  
+}
