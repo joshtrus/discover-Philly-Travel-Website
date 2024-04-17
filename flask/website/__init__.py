e7a9233a27d2b562142721bed1b423e7cd133ad2
jam993 <jam993@drexel.edu>
2024-04-16 22:07:03 -0400

    added more template pages

--- flask/website/__init__.py
+++ flask/website/__init__.py
@@ -4,4 +4,8 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] =  'cookie'
      
+    from .views import views 
+
+    app.register_blueprint(views, url_prefix = '/')
+
     return app
