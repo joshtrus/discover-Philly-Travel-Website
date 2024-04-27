63314f448ff7c51edecb9511c40820da7e1e0644
jam993 <jam993@drexel.edu>
2024-04-27 12:23:37 -0400

    Made changs to init.py in the flask folder. I am trying to connect the philly_travel database to my flask code

--- flask/website/__init__.py
+++ flask/website/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy  import SQLAlchemy
+import os
+
+db = SQLAlchemy( )
+DB_NAME = "philly_travel.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] =  'cookie'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'philly_travel.db')
      
     from .views import views 
 
