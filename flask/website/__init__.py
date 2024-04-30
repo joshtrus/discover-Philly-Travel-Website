ebf23e8f20e375157692d881ee3f2e78cd43f758
jam993 <jam993@drexel.edu>
2024-04-29 21:35:13 -0400

    still cant get to connect the database to the flask. editing the randomizer function now to show the name of the place. going to hard code the database for now

--- flask/website/__init__.py
+++ flask/website/__init__.py
@@ -8,7 +8,8 @@ DB_NAME = "philly_travel.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] =  'cookie'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'philly_travel.db')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'philly_travel.db')
+    db.init_app(app)
      
     from .views import views 
 
